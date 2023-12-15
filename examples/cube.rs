4804b9156d94bc5990366e78a6cbd02de30024f9
thatvertigo <56005814+thatvertigo@users.noreply.github.com>
2023-12-15 15:05:18 -0500

    Remove IPC on main

--- examples/cube.rs
+++ examples/cube.rs
@@ -1,30 +1,14 @@
 use bevy::prelude::*;
-use bevy_wry_webview::{
-    ipc::{SendBytes, WryMessage},
-    UiWebViewBundle, WebViewDespawning, WebViewHandle, WebViewLocation, WebViewMarker,
-    WebViewPlugin,
-};
+use bevy_wry_webview::{UiWebViewBundle, WebViewLocation, WebViewMarker, WebViewPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(WebViewPlugin)
-        .insert_resource(ButtonClickCount(0))
         .add_systems(Startup, setup)
-        //.add_systems(Update, moving_webview)
-        .add_systems(Update, button_system)
-        .add_systems(Update, handle_message)
+        .add_systems(Update, moving_webview)
         .run();
 }
-
-#[derive(Component)]
-struct ColoredCube;
-
-#[derive(Component)]
-struct CountView;
-
-#[derive(Resource)]
-struct ButtonClickCount(u8);
 
 /// set up a simple 3D scene
 fn setup(
@@ -32,35 +16,6 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(100.0),
-                align_items: AlignItems::FlexEnd,
-                justify_content: JustifyContent::Center,
-                ..default()
-            },
-            ..default()
-        })
-        .with_children(|parent| {
-            parent.spawn(ButtonBundle {
-                style: Style {
-                    width: Val::Px(150.0),
-                    height: Val::Px(65.0),
-                    border: UiRect::all(Val::Px(5.0)),
-                    // horizontally center child text
-                    justify_content: JustifyContent::Center,
-                    // vertically center child text
-                    align_items: AlignItems::Center,
-                    margin: UiRect::all(Val::Px(50.0)),
-                    ..default()
-                },
-                border_color: BorderColor(Color::BLACK),
-                background_color: NORMAL_BUTTON.into(),
-                ..default()
-            });
-        });
-
     commands.spawn(UiWebViewBundle {
         node_bundle: NodeBundle {
             style: Style {
@@ -73,28 +28,45 @@ fn setup(
             },
             ..Default::default()
         },
-        location: WebViewLocation::Html(include_str!("./cube.html").to_owned()),
+        location: WebViewLocation::Html(
+            r#"
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <div id="ele">
+            <div>Positioned element on transparent background</div>
+        </div>
+    </body>
+    <style>
+html, body {
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(255, 192, 203, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+#ele {
+    background-color: blue;
+    color: white;
+    width: 50%;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+    </style>
+</html>
+                      "#
+            .to_owned(),
+        ),
         ..Default::default()
     });
-
-    commands.spawn((
-        UiWebViewBundle {
-            node_bundle: NodeBundle {
-                style: Style {
-                    position_type: PositionType::Absolute,
-                    right: Val::Px(0.0),
-                    top: Val::Px(0.0),
-                    width: Val::Px(400.0),
-                    height: Val::Px(400.0),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            location: WebViewLocation::Html(include_str!("./cube1.html").to_owned()),
-            ..Default::default()
-        },
-        CountView,
-    ));
 
     // plane
     commands.spawn(PbrBundle {
@@ -103,15 +75,12 @@ fn setup(
         ..default()
     });
     // cube
-    commands.spawn((
-        PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(Color::rgb(0.5, 0.5, 0.5).into()),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        },
-        ColoredCube,
-    ));
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
@@ -140,55 +109,4 @@ fn moving_webview(time: Res<Time>, mut query: Query<&mut Style, With<WebViewMark
             ..style.clone()
         };
     });
-}
-
-const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
-
-fn button_system(
-    mut interaction_query: Query<
-        (&Interaction, &mut BackgroundColor, &mut BorderColor),
-        (Changed<Interaction>, With<Button>),
-    >,
-    query: Query<&WebViewHandle, With<CountView>>,
-    mut writer: EventWriter<SendBytes>,
-    mut counter: ResMut<ButtonClickCount>,
-) {
-    for (interaction, mut color, mut border_color) in &mut interaction_query {
-        match *interaction {
-            Interaction::Pressed => {
-                *color = PRESSED_BUTTON.into();
-                (*counter).0 += 1;
-                border_color.0 = Color::RED;
-                writer.send(SendBytes(query.single().clone(), vec![(*counter).0]));
-            }
-            Interaction::Hovered => {
-                *color = HOVERED_BUTTON.into();
-                border_color.0 = Color::WHITE;
-            }
-            Interaction::None => {
-                *color = NORMAL_BUTTON.into();
-                border_color.0 = Color::BLACK;
-            }
-        }
-    }
-}
-
-fn handle_message(
-    mut reader: EventReader<WryMessage>,
-    mut bg_color: Query<&mut Handle<StandardMaterial>, With<ColoredCube>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let mut color: Option<Color> = None;
-    for WryMessage(_, data) in &mut reader {
-        color = Some(Color::rgb(
-            data[0] as f32 / 255.0,
-            data[1] as f32 / 255.0,
-            data[2] as f32 / 255.0,
-        ))
-    }
-    if let Some(color) = color {
-        *bg_color.single_mut() = materials.add(color.into());
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
-use ipc::{WebViewIpcPlugin, WryFetchSender, WryMessageSender};
 use reactivity::WebViewReactivityPlugin;
-use wry::{
-    http::{Method, Response},
-    WebView, WebViewBuilder,
-};
+use wry::{WebView, WebViewBuilder};
 
 use bevy::{
     prelude::*,
@@ -11,7 +7,6 @@ use bevy::{
 };
 use raw_window_handle::{ActiveHandle, WindowHandle};
 
-pub mod ipc;
 mod reactivity;
 
 pub struct WebViewPlugin;
@@ -39,7 +34,6 @@ pub struct UiWebViewBundle {
     pub location: WebViewLocation,
     pub handle: WebViewHandle,
     pub marker: WebViewMarker,
-    // TODO Add IPC handler
 }
 
 impl Default for UiWebViewBundle {
@@ -82,7 +76,7 @@ impl WebViewDespawning for Commands<'_, '_> {
 impl Plugin for WebViewPlugin {
     fn build(&self, app: &mut App) {
         app.insert_non_send_resource(WebViewRegistry { webviews: vec![] })
-            .add_plugins((WebViewReactivityPlugin, WebViewIpcPlugin))
+            .add_plugins(WebViewReactivityPlugin)
             .add_systems(Update, Self::on_webview_spawn);
     }
 }
@@ -100,8 +94,6 @@ impl WebViewPlugin {
             ),
             With<WebViewMarker>,
         >,
-        sender: Res<WryMessageSender>,
-        fsender: Res<WryFetchSender>,
     ) {
         if let Ok(window_handle) = window_handle.get_single().map(|x| x.window_handle) {
             for (mut handle, location, size, position) in
@@ -116,66 +108,12 @@ impl WebViewPlugin {
 
                 *handle = WebViewHandle(Some(registry.len()));
 
-                let sender_cloned = sender.clone();
-                let fsender_cloned = fsender.clone();
-                let len = registry.len();
-                let (data_tx, data_rx) = crossbeam::channel::unbounded();
-                let (protocol_tx, protocol_rx) = crossbeam::channel::unbounded();
-
                 let borrowed_handle =
                     unsafe { &WindowHandle::borrow_raw(window_handle, ActiveHandle::new()) };
                 let webview = WebViewBuilder::new_as_child(&borrowed_handle)
                     .with_position(final_position)
                     .with_transparent(true)
-                    .with_size((size.x as u32, size.y as u32))
-                    .with_initialization_script(WebViewIpcPlugin::IPC_INIT_SCRIPT)
-                    .with_asynchronous_custom_protocol(
-                        "bevy".to_owned(),
-                        move |req, res| {
-                            let _ = protocol_tx.send((req, res)).unwrap();
-                        }, //WebViewIpcPlugin::handle_ipc,
-                    );
-
-                // async custom protocol thread
-                std::thread::spawn(move || {
-                    while let Ok((req, res)) = protocol_rx.recv() {
-                        if req.uri() == "bevy://send"
-                            || req.uri() == "bevy://send/" && req.method() == Method::POST
-                        {
-                            let _ = sender_cloned
-                                .send((WebViewHandle(Some(len)), req.body().to_owned()));
-                            res.respond(Response::builder().status(200).body(vec![]).unwrap());
-                        } else if req.uri().to_string().starts_with("bevy://fetch/")
-                            && req.method() == Method::GET
-                        {
-                            match req.uri().to_string().split_at(13).1.parse::<u128>() {
-                                Ok(x) => {
-                                    let _ = fsender_cloned.send((
-                                        WebViewHandle(Some(len)),
-                                        x,
-                                        data_tx.clone(),
-                                    ));
-
-                                    match data_rx.recv() {
-                                        Ok(data) if !data.is_empty() => res.respond(
-                                            Response::builder().status(200).body(data).unwrap(),
-                                        ),
-                                        _ => res.respond(
-                                            Response::builder().status(404).body(vec![]).unwrap(),
-                                        ),
-                                    }
-                                }
-                                Err(_) => {
-                                    res.respond(
-                                        Response::builder().status(409).body(vec![]).unwrap(),
-                                    );
-                                }
-                            }
-                        } else {
-                            res.respond(Response::builder().status(404).body(vec![]).unwrap());
-                        }
-                    }
-                });
+                    .with_size((size.x as u32, size.y as u32));
 
                 let webview = match location {
                     WebViewLocation::Url(url) => webview.with_url(url),
